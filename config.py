1521f578a92db57d1b9d9f4d4c539be68162c211
ibrahimahmet994 <dursunibrahimahmet@gmail.com>
2026-01-01 06:00:49 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,25 @@
+# Discord bot token
+BOT_TOKEN = ""
+
+# Log kanalı ID
+LOG_CHANNEL_ID = 1394835843629318326
+
+# Hedef sunucu ve rol ID'leri
+TARGETS = [
+    {"guild_id": 1444327699341381765, "role_id": 1445145586289479690},
+    # Gerekirse buraya başka sunucular / roller ekle
+]
+
+# Sabit link
+INVITE_LINK = "https://discord.gg/fJzKU5VX"
+
+# Senin User ID
+MY_USER_ID = 1311072938757328927
+
+# Batch ayarları
+BATCH_SIZE = 10          # Her batch maksimum 10 kişi
+DM_DELAY_RANGE = (2)   # Her DM arası saniye
+BATCH_DELAY_RANGE = (5,10) # Batch arası saniye
+
+# Saatlik DM limiti (spam önleme)
+HOURLY_LIMIT = 100
